a61e2772734066b6c5f4b193fa2f8902cd42e475
NEOLabs-software <THAYEEBOI890@GMAIL.COM>
2026-06-02 19:15:17 -0500

    create version header file

---
+++ src/version.h
@@ -0,0 +1,44 @@
+/* version.h
+
+Copyright (C) 2026 Santiago Silva.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies of the Software and its documentation and acknowledgment shall be
+given in the documentation and software packages that this Software was
+used.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+#pragma once
+
+#define SEHN_VERSION        "0.1.0"
+#define SEHN_VERSION_MAJOR  0
+#define SEHN_VERSION_MINOR  1
+#define SEHN_VERSION_PATCH  0
+
+// set by meson via config.h
+#ifdef HAVE_LIBEXIF
+#  define SEHN_FEAT_EXIF    true
+#else
+#  define SEHN_FEAT_EXIF    false
+#endif
+
+#ifdef HAVE_LIBCURL
+#  define SEHN_FEAT_CURL    true
+#else
+#  define SEHN_FEAT_CURL    false
+#endif
